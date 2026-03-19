cad68c859245fe87efa3b9b0aa414c35f179482f
Andrei Lysenko <57161609+aply333@users.noreply.github.com>
2026-03-19 02:31:54 -0700

    Vervel var correction

--- src/lib/sanity/client.js
+++ src/lib/sanity/client.js
@@ -1,14 +1,25 @@
 import { createClient } from '@sanity/client';
 import { dev } from '$app/environment';
-import { env } from '$env/dynamic/public';
+import {
+	PUBLIC_SANITY_PROJECT_ID,
+	PUBLIC_SANITY_DATASET,
+	PUBLIC_SANITY_API_VERSION,
+	PUBLIC_SANITY_PERSPECTIVE,
+	PUBLIC_SANITY_USE_CDN
+} from '$env/static/public';
 
-const projectId = env.PUBLIC_SANITY_PROJECT_ID || 'zh3wqn4s';
-const dataset = env.PUBLIC_SANITY_DATASET || 'production';
-const apiVersion = env.PUBLIC_SANITY_API_VERSION || '2026-03-18';
-const perspective = env.PUBLIC_SANITY_PERSPECTIVE || 'published';
-const useCdn = env.PUBLIC_SANITY_USE_CDN
-	? env.PUBLIC_SANITY_USE_CDN === 'true'
+const projectId = PUBLIC_SANITY_PROJECT_ID;
+const dataset = PUBLIC_SANITY_DATASET;
+const apiVersion = PUBLIC_SANITY_API_VERSION || '2026-03-18';
+const perspective = PUBLIC_SANITY_PERSPECTIVE || 'published';
+
+const useCdn = PUBLIC_SANITY_USE_CDN
+	? PUBLIC_SANITY_USE_CDN === 'true'
 	: !dev;
+
+if (!projectId || !dataset) {
+	throw new Error('Sanity env variables missing');
+}
 
 export const sanityClient = createClient({
 	projectId,
